b322f8a79fd6cea5cc6bc499c4c04d413ab658f8
karthik <karthikbangera87@gmail.com>
2014-05-28 20:40:41 -0400

    E-commerce code push
    E-commerce front end code push

---
+++ js/modern-business.js
@@ -0,0 +1,24 @@
+// Activates the Carousel
+$('.carousel').carousel({
+  interval: 5000
+})
+
+// Activates Tooltips for Social Links
+$('.tooltip-social').tooltip({
+  selector: "a[data-toggle=tooltip]"
+})
+
+$.fn.stars = function() {
+    return $(this).each(function() {
+        // Get the value
+        var val = parseFloat($(this).html());
+        // Make sure that the value is in 0 - 5 range, multiply to get width
+        var size = Math.max(0, (Math.min(5, val))) * 16;
+        // Create stars holder
+        var $span = $('<span />').width(size);
+        // Replace the numerical value with stars
+        $(this).html($span);
+    });
+}
+
+$(function() {$('span.stars').stars();});
